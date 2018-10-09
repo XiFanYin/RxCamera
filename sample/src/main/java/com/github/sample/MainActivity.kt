00062ffd9c791234f340fc38a87d63ccadffcf6c
qingmei2 <mq2553299@qq.com>
2018-10-09 14:03:15 +0800

    Add the RxCamera library module.

--- sample/src/main/java/com/github/sample/MainActivity.kt
+++ sample/src/main/java/com/github/sample/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.github.rxcamera.rxcamera
+package com.github.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
